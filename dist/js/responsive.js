a292083ad4102735bb3dbdb9ed71e2ae8af5cd29
Veselin Angelov <veskbg@gmail.com>
2020-06-08 03:32:50 +0300

    scrolling update

--- dist/js/responsive.js
+++ dist/js/responsive.js
@@ -5,17 +5,35 @@ function responsiveMenu() {
     if (x.className === "list-menu") {
         x.className += " responsive-menu";
         y.className += " responsive-head";
+        document.body.style.position = "fixed";
     } else {
         x.className = "list-menu";
         y.className = "site-header";
-        document.getElementById("site-header-id").style.position = "initial";
+        document.body.style.position = "";
+    }
+}
+
+function responsiveMenuMain() {
+    var x = document.getElementById("list-menu-id");
+    var y = document.getElementById("site-header-id");
+    if (x.className === "list-menu") {
+        x.className += " responsive-menu";
+        y.className += " responsive-head";
+        document.body.style.position = "fixed";
+    } else {
+        x.className = "list-menu";
+        y.className = "site-header site-header-main";
+        document.body.style.position = "";
     }
 }
 
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-    var is_mobile = document.getElementsByClassName("site-header responsive-head")[0];
-    if (is_mobile.className === "site-header responsive-head") {
-        document.getElementById("site-header-id").style.position = "fixed";
-    }
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("site-header-id").style.top = "0";
+  } else {
+    document.getElementById("site-header-id").style.top = "-78px";
+  }
+  prevScrollpos = currentScrollPos;
 };
